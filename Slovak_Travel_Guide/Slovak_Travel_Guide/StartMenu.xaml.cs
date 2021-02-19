417948146d392c8f06618ee8349c038abf811051
Jaroslav Ondruš <jaroslavondrus04@gmail.com>
2021-02-19 10:02:59 -0800

    Add project files.

---
+++ Slovak_Travel_Guide/Slovak_Travel_Guide/StartMenu.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Slovak_Travel_Guide
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartMenu : ContentPage
+    {
+        public StartMenu()
+        {
+            InitializeComponent();
+        }
+
+        async void btnTravelClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage());
+        }
+    }
+}
