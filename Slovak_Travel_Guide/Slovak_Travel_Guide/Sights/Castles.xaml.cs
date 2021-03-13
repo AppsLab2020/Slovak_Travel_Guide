3f1687da0d48f27947875b689af36c8d12cbdce4
Jaroslav Ondruš <jaroslavondrus04@gmail.com>
2021-03-13 19:38:27 +0100

    Add 2 more options of navigaion on castles.xaml site

--- Slovak_Travel_Guide/Slovak_Travel_Guide/Sights/Castles.xaml.cs
+++ Slovak_Travel_Guide/Slovak_Travel_Guide/Sights/Castles.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -22,6 +22,30 @@ namespace Slovak_Travel_Guide.Sights
         async void BtnScrollUpClicked(object sender, EventArgs e)
         {
            // await SVCastles.ScrollToAsync(0, LblCastles.Y, true);
+        }
+
+        private async void NavigateGpsBratislavskyHrad(object sender, EventArgs e)
+        {
+            double latitude = 48.142248;
+            double longitude = 17.0996481;
+
+            await Map.OpenAsync(latitude, longitude, new MapLaunchOptions
+            {
+                Name = "Bratislavsky Hrad",
+                NavigationMode = NavigationMode.Driving,
+            });
+        }
+
+        private async void NavigateGpsOravskyHrad(object sender, EventArgs e)
+        {
+            double latitude = 49.2620572;
+            double longitude = 19.358658;
+
+            await Map.OpenAsync(latitude, longitude, new MapLaunchOptions
+            {
+                Name = "Oravsky Hrad",
+                NavigationMode = NavigationMode.Driving,
+            });
         }
     }
 }
