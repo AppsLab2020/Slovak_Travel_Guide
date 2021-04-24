311782fd9fc6d56eaf97e4b76ea484c5f69e9880
Jaroslav Ondruš <jaroslavondrus04@gmail.com>
2021-04-24 19:53:22 +0200

    added some new features

--- Slovak_Travel_Guide/Slovak_Travel_Guide/Service/SightsService.cs
+++ Slovak_Travel_Guide/Slovak_Travel_Guide/Service/SightsService.cs
@@ -18,8 +18,8 @@ namespace Slovak_Travel_Guide.Service
         {
             return new ObservableCollection<CavesModel>
             {
-                new CavesModel(){MainUrl="https://bratislavskykraj.sk/wp-content/uploads/2020/05/str-38_hrad-696x626.jpg", AboutSight="Jop more pjekny to je id tam a neotravuj",Name="Bratislavsky hrad", SecondUrl="https://turisticky.sk/wp-content/uploads/2020/01/Bratislavsky-hrad_SS_02-scaled.jpg", ThirdUrl="https://turisticky.sk/wp-content/uploads/2020/01/Bratislavsky-hrad_SS_02-scaled.jpg", Latitude=48.142248, Longtitude=17.0996481, IsVisible=false, },
-                new CavesModel(){MainUrl="https://bratislavskykraj.sk/wp-content/uploads/2020/05/str-38_hrad-696x626.jpg", AboutSight="Jop more pjekny to je id tam a neotravuj",Name="Bratislavsky hrad", SecondUrl="https://turisticky.sk/wp-content/uploads/2020/01/Bratislavsky-hrad_SS_02-scaled.jpg", ThirdUrl="https://turisticky.sk/wp-content/uploads/2020/01/Bratislavsky-hrad_SS_02-scaled.jpg", Latitude=48.142248, Longtitude=17.0996481, IsVisible=false, }
+                new CavesModel(){MainUrl="https://bratislavskykraj.sk/wp-content/uploads/2020/05/str-38_hrad-696x626.jpg", AboutSight="Jop more pjekny to je id tam a neotravuj",Name="Bratislavský hrad", SecondUrl="https://turisticky.sk/wp-content/uploads/2020/01/Bratislavsky-hrad_SS_02-scaled.jpg", ThirdUrl="https://turisticky.sk/wp-content/uploads/2020/01/Bratislavsky-hrad_SS_02-scaled.jpg", Latitude=48.142248, Longtitude=17.0996481, IsVisible=false, },
+                new CavesModel(){MainUrl="https://bratislavskykraj.sk/wp-content/uploads/2020/05/str-38_hrad-696x626.jpg", AboutSight="Jop more pjekny to je id tam a neotravuj",Name="Bratislavský hrad", SecondUrl="https://turisticky.sk/wp-content/uploads/2020/01/Bratislavsky-hrad_SS_02-scaled.jpg", ThirdUrl="https://turisticky.sk/wp-content/uploads/2020/01/Bratislavsky-hrad_SS_02-scaled.jpg", Latitude=48.142248, Longtitude=17.0996481, IsVisible=false, }
             };
         }
         public List<MountainsModel> GetListMountains()
--- Slovak_Travel_Guide/Slovak_Travel_Guide/View/Caves.xaml.cs
+++ Slovak_Travel_Guide/Slovak_Travel_Guide/View/Caves.xaml.cs
@@ -1,6 +1,7 @@
 ﻿
 using Slovak_Travel_Guide.Model;
 using Slovak_Travel_Guide.ViewModel;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -18,13 +19,20 @@ namespace Slovak_Travel_Guide.Sights
             BindingContext = new CavesViewModel();
         }
        private void ListView_OnItemTapped(object sender, ItemTappedEventArgs e)
-        {
+       {
             var vm = BindingContext as CavesViewModel;
 
             var cave = e.Item as CavesModel;
 
             vm.HideOrShowCaves(cave);
             vm.FillCommandGPS(cave);
+       }
+
+        [Obsolete]
+        private void BtnInfoClicked(object sender, EventArgs e)
+        {
+            var url = "https://sk.wikipedia.org/wiki/Bratislavsk%C3%BD_hrad";
+            Device.OpenUri(new Uri(url));
         }
     }
 }
