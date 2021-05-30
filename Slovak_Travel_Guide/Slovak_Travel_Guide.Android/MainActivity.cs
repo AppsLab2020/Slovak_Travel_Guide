62a971232af3bc76a5ca68a6211835cf225ba4be
Jaroslav Ondruš <jaroslavondrus04@gmail.com>
2021-05-30 14:51:41 +0200

    add a new icon

--- Slovak_Travel_Guide/Slovak_Travel_Guide.Android/MainActivity.cs
+++ Slovak_Travel_Guide/Slovak_Travel_Guide.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Slovak_Travel_Guide.Droid
 {
-    [Activity(Label = "Slovak_Travel_Guide", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Slovak_Travel_Guide", Icon = "@mipmap/compass_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
